b6c8d35b8fad6481e7a5830bf064e47a816a48d4
Jayasham19 <muon544@gmail.com>
2025-09-12 00:47:16 +0530

    Initial commit ignoring node_modules

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+const expressLayouts = require("express-ejs-layouts");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(expressLayouts);
+// express layout did it automatically crying emojiiii
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.set("layout", "layouts/boilerplate");
+
+app.get("/CFC", (req, res) => {
+  res.render("index");
+});
+
+
+app.get("/CFC/calculate", (req, res) => {
+  res.render("calculate");
+});
+
+app.post("/calculate", (req, res) => {
+  const { vehicleType, distance, electricity, cookingFuel, dietType, waste, recycle } = req.body;
+
+  let footprint = 0;
+  let transportFactor = 0;
+  if (vehicleType === "car") transportFactor = 0.2;
+  if (vehicleType === "bike") transportFactor = 0.1;
+  if (vehicleType === "bus") transportFactor = 0.1;
+  if (vehicleType === "train") transportFactor = 0.04;
+  if (vehicleType === "plane") transportFactor = 0.25;
+  footprint += (parseFloat(distance) || 0) * 52 * transportFactor;
+  footprint += (parseFloat(electricity) || 0) * 12 * 0.9;
+  footprint += (parseFloat(cookingFuel) || 0) * 12 * 41;
+  if (dietType === "veg") footprint += 1700;
+  else if (dietType === "nonveg") footprint += 2500;
+  else if (dietType === "vegan") footprint += 1500;
+  else if (dietType === "mixed") footprint += 2000;
+  let wasteEmission = (parseFloat(waste) || 0) * 52 * 0.45;
+  if (recycle === "yes") wasteEmission *= 0.7;
+  footprint += wasteEmission;
+
+   res.render("result", { footprint: footprint.toFixed(2) });
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/learn", (req, res) => {
+  res.render("learn");
+});
+
+app.listen(port, () => {
+    console.log("Server Connected");
+  console.log(` Server is running at http://localhost:${port}/CFC`);
+});
